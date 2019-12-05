bd57ade9a503b0d30f385f36e85c9923bf5a080c
Admin <Admin@192.168.0.138>
2019-12-05 15:25:11 +0530

    changed

--- coreexample/src/coreexample/replacefirst.java
+++ coreexample/src/coreexample/replacefirst.java
@@ -4,8 +4,8 @@ public class replacefirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = new String("ishu.com");
-		System.out.println(str.replaceFirst("ishu", "ish"));
+		String str = new String("sweety.com");
+		System.out.println(str.replaceFirst("sweety", "ish"));
 		System.out.println(str.replaceFirst("ishu", "com"));
 
 	}
